a004fa3ae23f0e0d77de450176f6038e20bee8d6
Embla Andersson <emblaandersson@yahoo.se>
2024-12-18 13:28:53 +0100

    added escape to modal window

--- src/code.js
+++ src/code.js
@@ -1,8 +1,9 @@
-const menuButton = document.getElementById("menuButton");
+// 3.
+const menuOpen = document.getElementById("menuButton");
 const menuClose = document.getElementById("menuClose");
 const menuContent = document.getElementById("menuContent");
 
-menuButton.addEventListener("click", () => {
+menuOpen.addEventListener("click", () => {
   menuContent.classList.toggle("hidden");
   menuClose.classList.toggle("hidden");
 });
@@ -10,4 +11,46 @@ menuButton.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   menuContent.classList.toggle("hidden");
   menuClose.classList.toggle("hidden");
+});
+
+// 1.
+
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("accessibleModal");
+let lastFocusedElement;
+
+function openModal() {
+  lastFocusedElement = document.activeElement;
+  modal.classList.remove("hidden");
+  modal.setAttribute("aria-hidden", "false");
+  closeModalBtn.focus();
+  document.addEventListener("keydown", handleModalKeyDown);
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  modal.setAttribute("aria-hidden", "true");
+  lastFocusedElement.focus();
+  document.removeEventListener("keydown", handleModalKeyDown);
+}
+
+function handleModalKeyDown(event) {
+  if (event.key === "Escape") closeModal();
+}
+
+openModalBtn.addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
+
+const dialog = document.getElementById("accessibleDialog");
+const openDialog = document.getElementById("openDialogBtn");
+const closeDialog = document.getElementById("closeDialogBtn");
+
+openDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+  dialog.close();
 });
